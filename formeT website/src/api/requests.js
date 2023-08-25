f2f4bdc5fff3928c016dcceeead1f50392e08652
MJ <127729507+MINGKYWILL@users.noreply.github.com>
2023-08-25 17:06:28 +0900

    update API
    update API

--- formeT website/src/api/requests.js
+++ formeT website/src/api/requests.js
@@ -2,11 +2,11 @@ const requests = {
   fetchNowPlaying: "/movie/now_playing",
   fetchTrending: "/trending/all/week",
   fetchTopRated: "/movie/top_rated",
-  fetchActionMovies: "/discover/movie? with_genres=28&page=6",
-  fetchFamilyMovies: "/discover/movie? with_genres=10751&page=2",
+  fetchActionMovies: "/discover/movie?with_genres=28&page=6",
+  fetchFamilyMovies: "/discover/movie?with_genres=10751&page=2",
   fetchComedyMovies: "/discover/movie?with_genres=35&page=2",
   fetchMysteryMovies: "/discover/movie?with_genres=9648&page=3",
-  fetchTv: "/discover/tv",
+  fetchTv: "/trending/tv/day",
   fetchAll: "/trending/all/day",
 };
 
